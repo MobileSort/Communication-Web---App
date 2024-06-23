385243f7a3c340b915901609e62be381c4c130f4
Cauan <cauan.henriquegarcia@gmail.com>
2024-06-23 20:00:59 -0300

    allow all origins

--- Phone.Connector.Faker/Program.cs
+++ Phone.Connector.Faker/Program.cs
@@ -5,6 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
