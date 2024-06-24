caab0c4762f2ef75d0bc0e612f2d4d721f9a2fbe
Cauan <cauan.henriquegarcia@gmail.com>
2024-06-23 22:54:06 -0300

    ok, now it is accepting all origins

--- Phone.Connector.Faker/Program.cs
+++ Phone.Connector.Faker/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseCors("AllowAllOrigins");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
